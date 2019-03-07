41dfb8382c3e9665328944dffd2148a33957ad1e
MooDY <muhammad.ibrahim0x@gmail.com>
2019-03-07 19:11:58 +0100

    sasa

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  test
+//
+//  Created by MooDY on 10/10/18.
+//  Copyright © 2018 MooDY. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a[8] = {1,0,1,1,0,0,1,1};
+    int recValue = 0;
+    
+    return 0;
+}
